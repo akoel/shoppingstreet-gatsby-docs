f71adaa092fc485b6827b3f1cb9853a1da10d0d8
Arian Koelewijn <ariankoelewijn@Arians-MacBook-Pro.local>
2022-04-06 22:58:49 +0200

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: "smooth-doc",
+      options: {
+        name: "Shoppingstreet documentation",
+        description: "Our philophy, our instructions, and a lot more...",
+        siteUrl: "https://docs.shoppingstreet.io",
+      },
+    },
+  ],
+};
