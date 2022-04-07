c09a600afc968356dc9fe9b61ef4f68160d58527
Arian Koelewijn <ariankoelewijn@Arians-MacBook-Pro.local>
2022-04-07 02:24:38 +0200

    set up van eerste data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  siteMetadata: {
+    siteUrl: "https://docs.shoppingstreet.io/",
+    title: "Shoppingstreet | Documentation",
+    author: `Shoppingstreet LLC`,
+    description: "A webshop solution creator with Shopify, BigCommerce, Stripe, Gatsbyjs, Nextjs, Netlify, Vercel, Contentful and Mailchimp. We work on great SEO and stellar Lighthouse Scores.",
+  },
   plugins: [
     {
       resolve: "smooth-doc",
@@ -6,6 +12,20 @@ module.exports = {
         name: "Shoppingstreet documentation",
         description: "Our philophy, our instructions, and a lot more...",
         siteUrl: "https://docs.shoppingstreet.io",
+        navItems: [
+          { title: 'Docs', url: '/docs/' },
+          { title: 'Shoppingstreet', url: 'https://www.shoppingstreet.io' }
+        ],
+        sections: ['About', 'Philosophy', 'Vendors', 'Servicelevel'],
+        githubRepositoryURL: 'https://github.com/akoel',
+        twitterAccount: 'shoppingstreet-io',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: 'G-T7EMD5YLYP',
       },
     },
   ],
---
+++ src/smooth-doc/theme.js
@@ -0,0 +1,7 @@
+// src/smooth-doc/theme.js
+import { theme as baseTheme } from 'smooth-doc/src/theme'
+
+export const theme = {
+  ...baseTheme,
+  // Custom theme
+}
