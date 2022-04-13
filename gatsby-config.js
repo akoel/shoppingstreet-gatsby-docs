2895ae971c46a13b27c809580ce60d5a69b9aecc
Arian Koelewijn <ariankoelewijn@Arians-MacBook-Pro.local>
2022-04-14 00:42:45 +0200

    added several docs

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,9 +16,18 @@ module.exports = {
           { title: 'Docs', url: '/docs/' },
           { title: 'Shoppingstreet', url: 'https://www.shoppingstreet.io' }
         ],
-        sections: ['About', 'Philosophy', 'Vendors', 'Servicelevel'],
+        sections: ['About', 'Philosophy', 'Vendors', 'Servicelevel', 'Disclaimer'],
         githubRepositoryURL: 'https://github.com/akoel',
         twitterAccount: 'shoppingstreet-io',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-crisp-chat',
+      options: {
+        websiteId: process.env.CRISP_WEBSITE_ID,
+        enableDuringDevelop: true, // Optional. Disables Crisp Chat during gatsby develop. Defaults to true.
+        defer: true, // Optional. Sets the Crisp loading script to defer instead of async. Defaults to false.
+        enableImprovedAccessibility: false // Optional. Sets aria-label attribute on pop-up icon for screen readers. Defaults to true.
       },
     },
     {
